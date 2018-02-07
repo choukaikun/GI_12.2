81866d5587a7e41ab62b03d381b297b528bee743
choukaikun <kaikun@me.com>
2018-02-07 00:55:28 -0600

    Update box_prep_sol.sh

--- box_prep_sol.sh
+++ box_prep_sol.sh
@@ -13,28 +13,42 @@ mkdir -p /opt/apps/users
 # Add groups
 groupadd -g 54321 dba
 groupadd -g 54322 oinstall
+groupadd -g 54323 asmadmin
+groupadd -g 54324 oper
+groupadd -g 54326 asmoper
+groupadd -g 54327 asmdba
 
-useradd -u 54321 -g oinstall -G dba -d /opt/apps/users/grid -m grid
+useradd -u 54321 -g oinstall -G dba,asmadmin,asmoper,asmdba -d /opt/apps/users/grid -m grid
+useradd -u 54322 -g oinstall -G dba,asmdba -d /opt/apps/users/oracle -m oracle
 # Update passwd file for auto home entry (/home/grid)
 # Add to /etc/auto_home:
 # *       localhost:/opt/apps/users/&
 
 # Add to grid user .profile:
-# umask 022
-# export ORACLE_BASE=/u01/app/grid
-# export ORACLE_HOME=/u01/app/12.1.0.1/grid/
-# export ORACLE_SID=+ASM1
-# export TEMP=/tmp
-# export TMPDIR=/tmp
-# export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+cat << EOF >> ~grid/.profile
+
+umask 022
+export ORACLE_BASE=/u01/app/grid
+export ORACLE_HOME=/u01/app/12.2.0.1/grid/
+export ORACLE_SID=+ASM1
+export TEMP=/tmp
+export TMPDIR=/tmp
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib
  
 export PATH=$PATH:/usr/bin:/usr/sbin:$ORACLE_HOME/bin
+
+export OH=${ORACLE_HOME}
+export OB=${ORACLE_BASE}
+
+alias oh='cd ${OH}'
+alias ob='cd ${OB}'
  
 ulimit -t unlimited
 ulimit -f unlimited
 ulimit -d unlimited
 ulimit -s unlimited
 ulimit -v unlimited
+EOF
 
 # Update swap to make it at least 4.5G
 zfs set volsize=4.5G rpool/swap
@@ -45,7 +59,9 @@ zfs set volsize=4.5G rpool/swap
 ### Convert to ZFS
 zfs create -o mountpoint=/u01/app/12.2.0.1/grid rpool/grid_home
 zfs create -o mountpoint=/u01/app/grid rpool/grid_base
+zfs create -o mountpoint=/u01/app/oracle rpool/oracle_base
 chown -R grid:oinstall /u01
+chown oracle:oinstall /u01/app/oracle
 chmod -R 775 /u01
 
 # Set kernel tunables
@@ -54,3 +70,10 @@ projmod -sK "project.max-sem-nsems=(priv,512,deny)" user.grid
 projmod -sK "project.max-sem-ids=(priv,128,deny)" user.grid
 projmod -sK "project.max-shm-ids=(priv,128,deny)" user.grid
 projmod -sK "process.max-file-descriptor=(basic,1025,deny),(priv,65536,deny)" user.grid
+
+# 12.2 GA requires SCAN address to resolve in DNS.  Install dnsmasq to do this.
+pkg install dnsmasq
+# Add localhost to /etc/resolv.conf
+# nameserver 127.0.0.1
+# Enable dnsmasq
+svcadm enable dnsmasq
